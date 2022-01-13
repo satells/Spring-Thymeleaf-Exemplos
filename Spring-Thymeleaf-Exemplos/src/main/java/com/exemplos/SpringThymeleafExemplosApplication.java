ac373c8834d3a8ed349d480bc07443dbd078933f
satells <msergiost@hotmail.com>
2022-01-13 12:40:29 -0300

    Start

---
+++ Spring-Thymeleaf-Exemplos/src/main/java/com/exemplos/SpringThymeleafExemplosApplication.java
@@ -0,0 +1,13 @@
+package com.exemplos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafExemplosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafExemplosApplication.class, args);
+	}
+
+}
---
+++ Spring-Thymeleaf-Exemplos/src/test/java/com/exemplos/SpringThymeleafExemplosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafExemplosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
